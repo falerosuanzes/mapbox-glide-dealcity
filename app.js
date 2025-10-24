4ea25a2e074f54bc026e921ff47172cef5cd6955
falerosuanzes <pablo.mfalero@byteintelligence.com>
2025-10-24 12:36:02 -0400

    Add console log for app.js initialization

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log('app.js running');
 mapboxgl.accessToken = 'pk.eyJ1IjoicGZhbGVybyIsImEiOiJjbWg1MjM2aXgwMzlxMmpvYXl5amphcWJhIn0.efd7l1ZBRlEs6BFmPhigew';
 const map = new mapboxgl.Map({ container: 'map', style: 'mapbox://styles/mapbox/light-v11', center: [-97.5, 38.5], zoom: 3 });
 map.on('load', () => console.log('Base map loaded'));
